669726ecaa89c1ce125e22cc09d496bffbeea6c6
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-02-11 00:24:07 +0000

    done the fibonaci practice

---
+++ recursion/fibonaci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int f[10];
+int mfib(int n){
+    if(n<=1){
+        f[n] = n;
+     return n;
+    }else{
+        if (f[n-2] == -1){
+            f[n-2] = mfib(n-2);
+        } 
+        if(f[n-1] == -1){
+            f[n-1] = mfib(n-1);
+
+            
+
+        }
+        return f[n-2] + f[n-1];
+        
+    }
+   
+}
+
+int main(){
+for (int i = 0; i < 10; ++i) {
+        f[i] = -1;
+    };
+    cout << mfib(10);
+    return 0;
+}
