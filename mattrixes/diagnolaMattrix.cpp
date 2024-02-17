02e19e3d2fabfae3cc3ae635390c5c6b2b7e914b
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-02-17 22:18:36 +0000

    nothing special

--- mattrixes/diagnolaMattrix.cpp
+++ mattrixes/diagnolaMattrix.cpp
@@ -15,39 +15,38 @@ void set(struct Mattrix *m, int i, int j, int x){
 
 };
 
-int get(struct Mattrix *m, int i, int j, int x){
-
-    if(i==j){
-        return m->A[i-1] = x;
-    }else{
+int Get(struct Mattrix *m, int i, int j) {
+    if (i == j) {
+        return m->A[i - 1];
+    } else {
         return 0;
     }
-
 };
-
 void Display(struct Mattrix m){
-    int i,j;
-    for(i=0;i<m.n;i++){
-        for (j=0;j<m.n;j++){
-            if ( i==j ){
-                cout<<m.A[i-1];
-            }else{
-                cout<< 0;
+    int i, j;
+    for(i = 1; i <= m.n; i++){
+        for (j = 1; j <= m.n; j++){
+            if (i == j){
+                cout << m.A[i-1];
+            } else {
+                cout << "0";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
+};
 
-}
 
 int main(){
     struct Mattrix m;
     m.n=4;
     set(&m,1,1,5);
-    set(&m,2,2,5);
-    set(&m,3,3,5);
-    set(&m,4,4,5);
+    set(&m,2,2,8);
+    set(&m,3,3,9);
+    set(&m,4,4,12);
+
+    cout<<Get(&m,1,1)<<endl;
     
     Display(m);
     return 0;
-}
+};
