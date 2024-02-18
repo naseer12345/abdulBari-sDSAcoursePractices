ac79cd5a492c0371d351f2c47285397a11343a44
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-02-18 18:58:05 +0000

    im not understanding any shit

--- mattrixes/Mattrixes.cpp
+++ mattrixes/Mattrixes.cpp
@@ -18,7 +18,7 @@
     }
     ~Diagonal()
     {
-        delete []A;
+        delete []A; 
     }
     void Set(int i,int j,int x);
     int Get(int i,int j);
@@ -53,10 +53,75 @@ cout<<endl;
  }
 
 
-
-
-
-int main(){
- 
+// lower triangular mattrix
+ class triagonalMattrix
+ {
+ private:
+    int *A;
+    int n;
+ public:
+    triagonalMattrix()
+    {
+        n=2;
+        A=new int[2*(2+1)/2];
+    }
+    triagonalMattrix(int n)
+    {
+        this->n=n;
+        A=new int[n*(n+1)/2];
+    }
+    ~triagonalMattrix()
+    {
+        delete []A; 
+    }
+    void Set(int i,int j,int x);
+    int Get(int i,int j);
+    void Display();
+    int GetDimension(){return n;}
+ };
+ void triagonalMattrix::Set(int i,int j,int x){
+    if(i>=j){
+        A[i*(i-1)/2+j-1]=x;
+    }
+        
+ }
+ int triagonalMattrix::Get(int i,int j){
+    if(i>=j){
+        return A[i*(i-1)/2+j-1];
+        }
     return 0;
-};
+ }
+ 
+ void triagonalMattrix::Display(){
+
+for(int i=1;i<=n;i++){
+ for(int j=1;j<=n;j++){
+ if(i>=j){
+    cout<<A[i*(i-1)/2+j-1]<<" ";
+    }else{
+     cout<<"0 ";
+    }
+}
+cout<<endl;
+    }
+ }
+
+
+
+int main() {
+    int d;
+    cout << "Enter dimensions: ";
+    cin >> d;
+    triagonalMattrix lm(d);
+    int x;
+    cout << "Enter all elements:";
+    for (int i = 1; i <= d; i++) {
+        for (int j = 1; j <= d; j++) {
+            cin >> x;
+            lm.Set(i, j, x);
+        }
+    }
+    lm.Display();
+
+    return 0;
+}
